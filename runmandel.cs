ad2e3224ace2dbabe521f59599c6f107ff108338
Fabio Mascarenhas <mascarenhas@acm.org>
2008-02-05 01:16:00 +0000

    silly mandelbrot example
    darcs-hash:20080205011642-678b8-016d12ad0963f42a3fecebf50cedf564fe8dd0b5.gz

---
+++ runmandel.cs
@@ -0,0 +1,58 @@
+using Lua;
+using System;
+using mandelbrot;
+
+public class runmandel {
+  public static void Main(string[] args) {
+    Table env = new Table();
+    Symbol k;
+    object v;
+
+    k = Lua.Symbol.Intern("print");
+    v = new Print();
+    env[k] = v;
+
+    k = Lua.Symbol.Intern("setmetatable");
+    v = new SetMetatable();
+    env[k] = v;
+
+    k = Lua.Symbol.Intern("pairs");
+    v = new Pairs();
+    env[k] = v;
+
+    Lua.Symbol s = Lua.Symbol.Intern("tonumber");
+    k = s;
+    v = new ToNumber();
+    env[k] = v;
+
+    k = Lua.Symbol.Intern("os");
+    Table os = new Table();
+    v = os;
+    env[k] = v;
+    k = Lua.Symbol.Intern("clock");
+    v = new Clock();
+    os[k] = v;
+
+    k = Lua.Symbol.Intern("io");
+    Table io = new Table();
+    v = io;
+    env[k] = v;
+    k = Lua.Symbol.Intern("write");
+    v = new Write();
+    io[k] = v;
+
+    k = Lua.Symbol.Intern("math");
+    Table math = new Table();
+    v = math;
+    env[k] = v;
+    k = Lua.Symbol.Intern("floor");
+    v = new Floor();
+    math[k] = v;
+
+    function1 f = new function1();
+    f.Env = env;
+    f.InvokeS();
+    for(int i = 0; i <= System.GC.MaxGeneration; i++)
+      Console.WriteLine(System.GC.CollectionCount(i));
+  }
+}
